591cc0a8db4c07a41f23e55f20aac61728cbe897
Muhammad <mohamedar4477@gmail.com>
2024-09-05 16:54:05 +0200

    limit the number of images a user can upload to a maximum of 10

--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -22,6 +22,7 @@ class PostController extends Controller
     {
         $request->validate([
             'content' => 'nullable|string',
+            'images' => 'nullable|array|max:10',
             'images.*' => 'nullable|image|mimes:jpg,jpeg,png,gif|max:2048',
         ]);
     
